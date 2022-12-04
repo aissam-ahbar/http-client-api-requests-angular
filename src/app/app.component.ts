f50ee53d8a112ca8dc7c8b97204df5bb04bbba5c
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-04 20:56:27 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+const URL = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'http-client-api-requests-demo';
+export class AppComponent implements OnInit {
+  public result: Observable<any> = of({});
+
+  /**
+   * Inject HTTP Client in your component using Dependency Injection (DI)
+   */
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.result = this.httpClient.get(URL);
+  }
 }
